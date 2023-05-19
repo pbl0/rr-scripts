21f09a85b77791bb4165321c26f6fbe9d0372fb0
pablo <pabarragan94@gmail.com>
2023-05-19 20:48:08 +0200

    UPDATE REFFILL

--- scripts/better-index/BetterIndex.user.js
+++ scripts/better-index/BetterIndex.user.js
@@ -6,195 +6,195 @@
 // @version     1.1.0
 // @author      Pablo
 // @description Index list
+// @run-at document-end
 // @downloadURL https://github.com/pbl0/rr-scripts/raw/main/scripts/better-index/BetterIndex.user.js
 // ==/UserScript==
 
 /**
  * Use ViolentMonkey or Bromite
- * 
+ *
  * v1.1.0 - Major update: Port to bromite, entire refactor, remove jQuery usage and GM_values.
  * v1.0.4 - Minor fixes.
  * v1.0.3 - Visual fix.
  * v1.0.2 - Add/Remove button to add custom regions, change of custom region color.
  */
 
-var myRegions = getLocalStorageItem('myregions');
+var myRegions = getLocalStorageItem("myregions");
 
 if (!myRegions) {
-    myRegions = [];
-    setLocalStorageItem('myregions', JSON.stringify(myRegions));
+  myRegions = [];
+  setLocalStorageItem("myregions", JSON.stringify(myRegions));
 }
 
-
 setTimeout(() => {
-    console.log('betterIndex')
-    window.addEventListener('popstate', listener);
-    const pushUrl = (href) => {
-        history.pushState({}, '', href);
-        window.dispatchEvent(new Event('popstate'));
-    };
+  console.log("betterIndex");
+  window.addEventListener("popstate", listener);
+  const pushUrl = (href) => {
+    history.pushState({}, "", href);
+    window.dispatchEvent(new Event("popstate"));
+  };
 
-    listener();
+  listener();
 }, 1000);
 
 function listener() {
-    if (location.href.includes('#listed/country/-2/')) {
-        filterRegions();
-    } else if (location.href.includes('#map/details/')) {
-        regionPage();
-    }
+  if (location.href.includes("#listed/country/-2/")) {
+    filterRegions();
+  } else if (location.href.includes("#map/details/")) {
+    regionPage();
+  }
 }
 
 function filterRegions() {
-    var tableInterval = setInterval(function () {
-        var tableList = document.querySelector('#table_list');
-        if (tableList) {
-            clearInterval(tableInterval);
-            tableList.style.borderCollapse = 'collapse';
-            var indiex = 1;
-            var listLevels = Array.from(
-                document.querySelectorAll(
-                    'tr.list_link.header_buttons_hover>.list_level.tip.yellow'
-                )
-            ).reverse();
-            listLevels.forEach(function (level) {
-                var text = level.innerText;
-                var parent = level.parentElement;
-                if (text > indiex || text === '11') {
-                    indiex += 1;
-                    var rat = Math.floor(level.getAttribute('rat'));
-                    parent.insertAdjacentHTML('beforeend', '<td>' + rat + '</td>');
-                } else if (myRegions.includes(level.parentElement.getAttribute('user'))) {
-                    parent.style.color = '#54bb38';
-                    var rat = Math.floor(level.getAttribute('rat'));
-                    parent.insertAdjacentHTML('beforeend', '<td>' + rat + '</td>');
-                } else {
-                    parent.remove();
-                }
-            });
+  var tableInterval = setInterval(function () {
+    var tableList = document.querySelector("#table_list");
+    if (tableList) {
+      clearInterval(tableInterval);
+      tableList.style.borderCollapse = "collapse";
+      var indiex = 1;
+      var listLevels = Array.from(
+        document.querySelectorAll(
+          "tr.list_link.header_buttons_hover>.list_level.tip.yellow"
+        )
+      ).reverse();
+      listLevels.forEach(function (level) {
+        var text = level.innerText;
+        var parent = level.parentElement;
+        if (text > indiex || text === "11") {
+          indiex += 1;
+          var rat = Math.floor(level.getAttribute("rat"));
+          parent.insertAdjacentHTML("beforeend", "<td>" + rat + "</td>");
+        } else if (
+          myRegions.includes(level.parentElement.getAttribute("user"))
+        ) {
+          parent.style.color = "#54bb38";
+          var rat = Math.floor(level.getAttribute("rat"));
+          parent.insertAdjacentHTML("beforeend", "<td>" + rat + "</td>");
+        } else {
+          parent.remove();
         }
-    }, 500);
+      });
+    }
+  }, 500);
 }
 
 function addClickEvents() {
-    var indexInterval = setInterval(function () {
-        var mobBoxSlide = document.querySelector(
-            'div.mob_box_swipe_i_1.no_outline.slick-slide.slick-current.slick-active'
-        );
-        if (mobBoxSlide) {
-            clearInterval(indexInterval);
-            var mobBoxInner = mobBoxSlide.querySelector(
-                '.mob_box_inner.mob_box_4_clean.float_left.imp.tc'
-            );
-            mobBoxInner.addEventListener('click', function () {
-                slide_header(mobBoxInner.getAttribute('action'));
-            });
-        }
-    }, 500);
+  var indexInterval = setInterval(function () {
+    var mobBoxSlide = document.querySelector(
+      "div.mob_box_swipe_i_1.no_outline.slick-slide.slick-current.slick-active"
+    );
+    if (mobBoxSlide) {
+      clearInterval(indexInterval);
+      var mobBoxInner = mobBoxSlide.querySelector(
+        ".mob_box_inner.mob_box_4_clean.float_left.imp.tc"
+      );
+      mobBoxInner.addEventListener("click", function () {
+        slide_header(mobBoxInner.getAttribute("action"));
+      });
+    }
+  }, 500);
 }
 
 function getMyRegions() {
-    myRegions = getLocalStorageItem('myregions');
+  myRegions = getLocalStorageItem("myregions");
 }
 
 function saveMyRegions() {
-    setLocalStorageItem('myregions', myRegions);
+  setLocalStorageItem("myregions", myRegions);
 }
 
 function removeRegion() {
-    var regionId = location.href.split('details/')[1];
-    myRegions = myRegions.filter(function (e) {
-        return e !== regionId;
-    });
+  var regionId = location.href.split("details/")[1];
+  myRegions = myRegions.filter(function (e) {
+    return e !== regionId;
+  });
 }
 
 function addRegion() {
-    var regionId = location.href.split('details/')[1];
-    myRegions.push(regionId);
+  var regionId = location.href.split("details/")[1];
+  myRegions.push(regionId);
 }
 
 function regionPage() {
-    if (location.href.includes('//m.')) {
-        // addClickEvents(); // No longer needed. Links were added to the base game.
-        addButton(true);
-    } else {
-        var betterIndex = document.querySelector('#better-index');
-        if (betterIndex) {
-            betterIndex.remove();
-        }
-        addButton(false);
+  if (location.href.includes("//m.")) {
+    // addClickEvents(); // No longer needed. Links were added to the base game.
+    addButton(true);
+  } else {
+    var betterIndex = document.querySelector("#better-index");
+    if (betterIndex) {
+      betterIndex.remove();
     }
+    addButton(false);
+  }
 }
 
 function addButton(mobile) {
-    let selector;
-    if (mobile) {
-        selector = '#region_details_after > div > div';
-    } else {
-        selector = 'div.small.imp.tc>span';
+  let selector;
+  if (mobile) {
+    selector = "#region_details_after > div > div";
+  } else {
+    selector = "div.small.imp.tc>span";
+  }
+
+  var buttonInterval = setInterval(function () {
+    var elements = document.querySelectorAll(selector);
+    if (elements.length) {
+      clearInterval(buttonInterval);
+      const regionId = location.href.split("details/")[1];
+      let addRemoveText;
+      let addRemoveFunc;
+      let color;
+      if (myRegions.includes(regionId)) {
+        addRemoveFunc = removeRegion;
+        addRemoveText = "BetterIndex Remove";
+        color = "red";
+      } else {
+        addRemoveText = "BetterIndex Add";
+        addRemoveFunc = addRegion;
+        color = "green";
+      }
+      if (mobile) {
+        mobileButton(addRemoveText, color);
+      } else {
+        pcButton(addRemoveText, color);
+      }
+
+      var betterIndex = document.getElementById("better-index");
+      betterIndex.addEventListener("click", function () {
+        addRemoveFunc();
+        saveMyRegions();
+        setTimeout(() => {
+          location.reload();
+        }, 500);
+      });
     }
-
-    var buttonInterval = setInterval(function () {
-        var elements = document.querySelectorAll(selector);
-        if (elements.length) {
-            clearInterval(buttonInterval);
-            const regionId = location.href.split('details/')[1];
-            let addRemoveText;
-            let addRemoveFunc;
-            let color;
-            if (myRegions.includes(regionId)) {
-                addRemoveFunc = removeRegion;
-                addRemoveText = 'BetterIndex Remove';
-                color = 'red';
-            } else {
-                addRemoveText = 'BetterIndex Add';
-                addRemoveFunc = addRegion;
-                color = 'green';
-            }
-            if (mobile) {
-                mobileButton(addRemoveText, color);
-            } else {
-                pcButton(addRemoveText, color);
-            }
-
-            var betterIndex = document.getElementById('better-index');
-            betterIndex.addEventListener('click', function () {
-                addRemoveFunc();
-                saveMyRegions();
-                setTimeout(() => {
-                    location.reload();
-                }, 500);
-
-            });
-        }
-    }, 500);
+  }, 500);
 }
 
 function mobileButton(addRemoveText, color) {
-    var detailsAfter = document.getElementById('region_details_after');
-    detailsAfter.insertAdjacentHTML(
-        'beforeend',
-        `<div class="details_act_in"><div class="button_${color} tip region_details_move_map float_left" id="better-index">${addRemoveText}</div></div>`
-    );
+  var detailsAfter = document.getElementById("region_details_after");
+  detailsAfter.insertAdjacentHTML(
+    "beforeend",
+    `<div class="details_act_in"><div class="button_${color} tip region_details_move_map float_left" id="better-index">${addRemoveText}</div></div>`
+  );
 }
 
 function pcButton(addRemoveText, color) {
-    var impTcSmall = document.querySelector('.imp.tc.small');
-    var span = document.createElement('span');
-    span.innerHTML = `<div class="button_${color} region_details_move" id="better-index">${addRemoveText}</div>`;
-    impTcSmall.appendChild(span);
+  var impTcSmall = document.querySelector(".imp.tc.small");
+  var span = document.createElement("span");
+  span.innerHTML = `<div class="button_${color} region_details_move" id="better-index">${addRemoveText}</div>`;
+  impTcSmall.appendChild(span);
 }
 
 function getLocalStorageItem(key) {
-    try {
-        return JSON.parse(localStorage.getItem(key));
-    } catch (error) {
-        return []; s
-    }
-
-
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch (error) {
+    return [];
+    s;
+  }
 }
 
 function setLocalStorageItem(key, value) {
-    return localStorage.setItem(key, JSON.stringify(value));
+  return localStorage.setItem(key, JSON.stringify(value));
 }
--- scripts/refill-gold/mobile/RefillGold.user.js
+++ scripts/refill-gold/mobile/RefillGold.user.js
@@ -2,241 +2,266 @@
 // @name        RefillGoldScript
 // @namespace   https://pablob.eu/
 // @match       *://m.rivalregions.com/
-// @grant       GM_getValue
-// @grant       GM_setValue
-// @version     0.0.9
+// @version     0.1.0
 // @author      Pablo
-// @description just refills da gold
+// @description just refills the gold (MOBILE)
+// @run-at document-idle
 // @downloadURL https://github.com/pbl0/rr-scripts/raw/main/scripts/refill-gold/mobile/RefillGold.user.js
-// @require https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // ==/UserScript==
 
 /**
+ * v0.1.0 -> port to bromite & auto-updates state id from user profile page
  * v0.0.8 -> small fix
  * v0.0.7 -> Fix on request
  * */
 
 // State ID of your state:
-const myState = GM_getValue('myState'); //  "3006";
+var myState = localStorage.getItem("myState"); //  "3006";
 // Hours it should wait for next refill ( default 2 ):
-const hours = GM_getValue('hours');
+const hours = localStorage.getItem("hours");
 // If gold level is below this it will refill (only works in your current region) ( default 250 ):
-const threshold = GM_getValue('threshold');
+const threshold = localStorage.getItem("threshold");
 
-const showTable = GM_getValue('table', true);
+const showTable = localStorage.getItem("table", true);
 
 var tabla;
 
 // First time
 if (!myState) {
-    GM_setValue('myState', '3006');
-    const myState = GM_getValue('myState');
+  localStorage.setItem("myState", "3006");
+  const myState = localStorage.getItem("myState");
 }
 if (!hours) {
-    GM_setValue('hours', 2);
-    const hours = GM_getValue('hours');
+  localStorage.setItem("hours", 2);
+  const hours = localStorage.getItem("hours");
 }
 if (!threshold) {
-    GM_setValue('threshold', 250);
-    const threshold = GM_getValue('threshold');
+  localStorage.setItem("threshold", 250);
+  const threshold = localStorage.getItem("threshold");
 }
 
-var autoRefillInterval;
 const timePassed = hours * 3600000;
-$(document).ready(function () {
-    window.addEventListener('popstate', listener);
 
-    const pushUrl = (href) => {
-        history.pushState({}, '', href);
-        window.dispatchEvent(new Event('popstate'));
-    };
+window.addEventListener("popstate", listener);
 
-    listener();
-});
+const pushUrl = (href) => {
+  history.pushState({}, "", href);
+  window.dispatchEvent(new Event("popstate"));
+};
+
+listener();
 
 function listener() {
-    if (location.href.includes('#main')) {
-        mainPage();
-        console.log('#main');
+  if (location.href.includes("#main")) {
+    mainPage();
+  } else if (location.href.includes("#slide/profile/")) {
+    let profile_button = document.querySelector(
+      "#mob_menu > div.float_left.header_avatar.pointer.tc.mob_lower_one"
+    );
+    myId = profile_button.getAttribute("action").split("/")[2];
+    if (location.href.includes(myId)) {
+      profile_page();
     }
-    let lastRefill = localStorage.getItem('last_refill');
-    if (
-        JSON.parse(localStorage.getItem('is_my_state')) &&
-        (lastRefill == null || c() - lastRefill > timePassed)
-    ) {
-        refill_gold();
+  }
+  let lastRefill = localStorage.getItem("last_refill");
+  if (
+    localStorage.getItem("is_my_state") &&
+    (lastRefill == null || new Date().getTime() - lastRefill > timePassed)
+  ) {
+    refill_gold();
 
-        addTable();
-    }
-
-    if (autoRefillInterval !== undefined) {
-        clearInterval(autoRefillInterval);
-        console.log('autoRefill is off');
-        autoRefillInterval = undefined;
-    }
+    // addTable();
+  }
 }
 
 function refill_gold() {
-    // Fetch
+  // Fetch
 
-    fetch('https://m.rivalregions.com/parliament/donew/42/0/0', {
-        headers: {
-            accept: '*/*',
-            'accept-language': 'en-US,en;q=0.9',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'sec-ch-ua': '"Chromium";v="91", " Not;A Brand";v="99"',
-            'sec-ch-ua-mobile': '?1',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'x-requested-with': 'XMLHttpRequest',
-        },
-        referrer: 'https://m.rivalregions.com/',
-        referrerPolicy: 'strict-origin-when-cross-origin',
-        body: `tmp_gov='0'&c=${c_html}`,
-        method: 'POST',
-        mode: 'cors',
-        credentials: 'include',
+  fetch("https://m.rivalregions.com/parliament/donew/42/0/0", {
+    headers: {
+      accept: "*/*",
+      "accept-language": "en-US,en;q=0.9",
+      "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+      "sec-ch-ua": '"Chromium";v="91", " Not;A Brand";v="99"',
+      "sec-ch-ua-mobile": "?1",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-requested-with": "XMLHttpRequest",
+    },
+    referrer: "https://m.rivalregions.com/",
+    referrerPolicy: "strict-origin-when-cross-origin",
+    body: `tmp_gov='0'&c=${get_c()}`,
+    method: "POST",
+    mode: "cors",
+    credentials: "include",
+  })
+    .then((response) => {
+      response.text().then((text) => {
+        if (text == "no 2") {
+          localStorage.setItem("is_my_state", false);
+        } else if (text == "ok") {
+          localStorage.setItem("last_refill", new Date().getTime());
+          deleteTable();
+        }
+      });
     })
-        .then((response) => {
-            // console.log(response);
-            response.text().then((text) => {
-                console.log(text);
-                if (text == 'no 2') {
-                    localStorage.setItem('is_my_state', false);
-                    console.log('wrong state');
-                } else if (text == 'ok') {
-                    localStorage.setItem('last_refill', c());
-                    console.log('gold refilled');
-                    deleteTable();
-                }
-            });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function workPage() {
-    var total = $(
-        'div.mob_box_inner.mob_box_5_clean.float_left.imp.tc> div.yellow.small_box'
-    ).text();
-    if (
-        $('.mslide.yellow').html() < threshold &&
-        JSON.parse(localStorage.getItem('is_my_state')) &&
-        total != '2500/2500'
-    ) {
-        refill_gold();
-    }
+  var total = $(
+    "div.mob_box_inner.mob_box_5_clean.float_left.imp.tc> div.yellow.small_box"
+  ).text();
+  if (
+    $(".mslide.yellow").html() < threshold &&
+    localStorage.getItem("is_my_state") &&
+    total != "2500/2500"
+  ) {
+    refill_gold();
+  }
+}
+
+function workPage() {
+  var total = document.querySelector(
+    "div.mob_box_inner.mob_box_5_clean.float_left.imp.tc> div.yellow.small_box"
+  ).textContent;
+  if (
+    parseInt(document.querySelector(".mslide.yellow").innerHTML) < threshold &&
+    JSON.parse(localStorage.getItem("is_my_state")) &&
+    total !== "2500/2500"
+  ) {
+    refill_gold();
+  }
 }
 
 function mainPage() {
-    var mainPageInterval = setInterval(function () {
-        if ($('#mob_box_region_1').length && !$('#my_mob_box').length) {
-            clearInterval(mainPageInterval);
+  var mainPageInterval = setInterval(function () {
+    if (
+      document.getElementById("mob_box_region_1") &&
+      !document.getElementById("my_mob_box")
+    ) {
+      clearInterval(mainPageInterval);
 
-            if (
-                $('#mob_box_region_2')
-                    .attr('action')
-                    .replace('map/state_details/', '') == myState
-            ) {
-                if (!$('#my_refill').length) {
-                    addMenu(true);
-                    $('#my_refill').click(refill_gold);
-                    $('#auto_refill').click(autoRefill);
-                }
-
-                localStorage.setItem('is_my_state', true);
-            } else {
-                addMenu(false);
-                localStorage.setItem('is_my_state', false);
-            }
+      if (
+        document
+          .getElementById("mob_box_region_2")
+          .getAttribute("action")
+          .replace("map/state_details/", "") == myState
+      ) {
+        if (!document.getElementById("my_refill")) {
+          addMenu(true);
+          document
+            .getElementById("my_refill")
+            .addEventListener("click", refill_gold);
         }
-    });
+
+        localStorage.setItem("is_my_state", true);
+      } else {
+        addMenu(false);
+        localStorage.setItem("is_my_state", false);
+      }
+    }
+  });
+}
+
+function profile_page() {
+  let myStateDiv = document.querySelector("div.short_details:nth-child(3)");
+  let stateUrl = myStateDiv.getAttribute("action");
+  let stateId = stateUrl.split("/")[2];
+  if (myState != stateId) {
+    localStorage.setItem("myState", stateId);
+    myState = localStorage.getItem("myState");
+  }
 }
 
 function addMenu(isOn) {
-    let lastRefill = new Date(
-        JSON.parse(localStorage.getItem('last_refill'))
-    ).toLocaleString();
-    var buttonColor;
-    var buttonText;
-    if (isOn) {
-        buttonColor = 'link';
-        buttonText = 'Refill now';
-        if (showTable) {
-            addTable();
-        }
-    } else {
-        buttonColor = 'white';
-        buttonText = 'Not your state';
-        // autoRefillButton = "";
+  let lastRefill = new Date(
+    JSON.parse(localStorage.getItem("last_refill"))
+  ).toLocaleString();
+  var buttonColor;
+  var buttonText;
+  if (isOn) {
+    buttonColor = "link";
+    buttonText = "Refill now";
+    if (showTable) {
+      // addTable();
     }
-    const myVersion = GM_info.script.version;
-    $('.mob_box.mob_box_region_s').append(
-        `<div id="my_refill" class="button_${buttonColor} index_auto pointer mslide">${buttonText}</div>
-		<div class="tiny">Last refill: ${lastRefill} (state:${myState})
-		<span class='addit_2'> Script by @pablobls (v${myVersion}) </span>
-		</div>`
-    );
-}
-
-function autoRefill() {
-    console.log('auto refill on');
-    $('#auto_refill').removeClass('button_green').addClass('button_white');
-    autoRefillInterval = setInterval(function () {
-        refill_gold();
-    }, timePassed);
+  } else {
+    buttonColor = "white";
+    buttonText = "Not your state";
+    // autoRefillButton = "";
+  }
+  document.querySelector(".mob_box.mob_box_region_s").insertAdjacentHTML(
+    "beforeend",
+    `<div id="my_refill" class="button_${buttonColor} index_auto pointer mslide">${buttonText}</div>
+          <div class="tiny">Last refill: ${lastRefill} (state:${myState})
+          <span class='addit_2'> Script by @pablobls</span>
+          </div>`
+  );
 }
 
 function refillFromTable() {
-    var doRefill = false;
-    $('#list_tbody>tr').each(function () {
-        // console.log($(this).text());
+  var doRefill = false;
+  document.querySelectorAll("#list_tbody>tr").forEach(function (tr) {
+    var limitLeft = tr.querySelector("td:nth-child(6)").textContent;
 
-        var limitLeft = $(this).find('td:nth-child(6)').text();
-
-        if (limitLeft > 0) {
-            var explored = $(this).find('td:nth-child(3)').text();
-            if (explored <= threshold) {
-                doRefill = true;
-                console.log($(this).text());
-            }
-        }
-    });
-
-    if (doRefill) {
-        console.log('Refill (table)');
-        refill_gold();
-    } else {
-        console.log('No Refill (table)');
+    if (limitLeft > 0) {
+      var explored = tr.querySelector("td:nth-child(3)").textContent;
+      if (explored <= threshold) {
+        doRefill = true;
+      }
     }
+  });
+
+  if (doRefill) {
+    refill_gold();
+  } else {
+  }
 }
+
+function get_c() {
+  let c;
+  if (typeof c_html != "undefined") {
+    c = c_html;
+  } else {
+    c = document
+      .getElementsByTagName("script")[1]
+      .innerHTML.split("c : '")[1]
+      .split("'},\n")[0];
+  }
+  return c;
+}
+
 function addTable() {
-    // Fetch
-    if (!tabla) {
-        fetch(`/listed/stateresources/${myState}?c=${c_html}`, {
-            method: 'GET',
-        })
-            .then((response) => {
-                // console.log(response);
-                response.text().then((text) => {
-                    const html = $.parseHTML(text);
-                    tabla = $(html).find('table');
-                    $('.mob_box.mob_box_region_s').after(tabla);
-                    refillFromTable();
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    } else {
-        //console.log('table from cache', tabla)
-        $('.mob_box.mob_box_region_s').after(tabla);
-        // refillFromTable();
-    }
-}
+  // Fetch
 
+  if (!tabla) {
+    fetch(`/listed/stateresources/${myState}?c=${get_c()}`, {
+      method: "GET",
+    })
+      .then((response) => {
+        response.text().then((text) => {
+          const parser = new DOMParser();
+          const html = parser.parseFromString(text, "text/html");
+          tabla = html.querySelector("table");
+          document
+            .querySelector(".mob_box.mob_box_region_s")
+            .insertAdjacentElement("afterend", tabla);
+          refillFromTable();
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    document
+      .querySelector(".mob_box.mob_box_region_s")
+      .insertAdjacentElement("afterend", tabla);
+  }
+}
 function deleteTable() {
-    tabla = null;
+  tabla = null;
 }
